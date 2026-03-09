7bf28dbb1216a8a20beafd369c43cac462b73094
Fia XTrek <2183726074@qq.com>
2026-03-09 14:36:57 +0800

    complete

---
+++ acc.py
@@ -0,0 +1,9 @@
+import subprocess
+import os
+
+result = subprocess.run('bash -c "source /etc/network_turbo && env | grep proxy"', shell=True, capture_output=True, text=True)
+output = result.stdout
+for line in output.splitlines():
+    if '=' in line:
+        var, value = line.split('=', 1)
+        os.environ[var] = value
